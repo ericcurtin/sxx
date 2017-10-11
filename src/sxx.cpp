48f1ea96a95b7c63a86aaa16e51f9763204bc1bb
Eric Curtin <ericcurtin17@gmail.com>
2017-10-12 00:13:03 +0100

    ssh by default, also support scp, rsync, term modes

--- src/sxx.cpp
+++ src/sxx.cpp
@@ -1,4 +1,28 @@
-int main(int, char* argv[]) {
+#include <vector>
+#include <string>
 
+using std::vector;
+using std::string;
+
+vector<string> toVector(const char* charv[]) {
+  vector<string> ret;
+
+  for (++charv; *charv; ++charv) {
+    ret.push_back(*charv);
+  }
+
+  return ret;
+}
+
+int main(const int, const char* argv[]) {
+  vector<string> argV = toVector(argv);
+  string type = argV[1];
+
+  if (type == "ssh" || type == "scp" || type == "rsync" || type == "term") {
+    argV.erase(argV.begin());
+  }
+  else {
+    type == "ssh";
+  }
 }
 
