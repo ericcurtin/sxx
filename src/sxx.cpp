3450c54ab60a1c0d4c358d859e87a2de1c91d248
Eric Curtin <ericcurtin17@gmail.com>
2017-10-15 17:47:00 +0100

    Start implementing host_group identifier

--- src/sxx.cpp
+++ src/sxx.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+#include <iostream>
 
 using std::vector;
 using std::string;
@@ -16,13 +17,36 @@ vector<string> toVector(const char* charv[]) {
 
 int main(const int, const char* argv[]) {
   vector<string> argV = toVector(argv);
-  string type = argV[1];
+  string type = argV[0];
 
   if (type == "ssh" || type == "scp" || type == "rsync" || type == "term") {
     argV.erase(argV.begin());
   }
   else {
-    type == "ssh";
+    type = "ssh";
   }
-}
 
+  for(vector<string>::const_iterator i = argV.begin(); i != argV.end(); ++i) {
+    const string& thisStr = *i;
+    if (thisStr[0] == '-') {
+      continue;
+    }
+
+    size_t start = thisStr.find('@');
+    if (start == string::npos) {
+        start = 0;
+    }
+    else {
+        ++start;
+    }
+
+    size_t end = thisStr.find(':', start);
+    if (end == string::npos) {
+        end = thisStr.size();
+    }
+
+    const string host_group = thisStr.substr(start, end - start);
+    std::cout << host_group << "\n";
+  }
+
+}
