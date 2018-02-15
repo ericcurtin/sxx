9e62de5c86326cb543c486264c593cc4f2d82071
Eric Curtin <ericcurtin17@gmail.com>
2018-02-15 15:02:06 +0000

    Add term mode
    mainly for tailing

--- src/sxx.cpp
+++ src/sxx.cpp
@@ -11,6 +11,7 @@ using std::vector;
 using std::string;
 using std::ifstream;
 using std::cout;
+using std::cerr;
 
 using Poco::Pipe;
 using Poco::Process;
@@ -63,11 +64,13 @@ vector<string> get_hosts(const string& host_grp) {
 struct proc {
   const string host_;
   Pipe out_pipe_;
+  Pipe err_pipe_;
   const ProcessHandle proc_hand_;
 
-  proc(const string& host, const string& type, const vector<string>& args)
+  proc(const string& host, const string& command, const vector<string>& args)
       : host_(host),
-        proc_hand_(Process::launch(type, args, nullptr, &out_pipe_, nullptr)) {}
+        proc_hand_(
+            Process::launch(command, args, nullptr, &out_pipe_, &err_pipe_)) {}
 };
 
 void grp_cmd(const string& type,
@@ -85,27 +88,38 @@ void grp_cmd(const string& type,
   vector<proc> procs;
   for (const string& host : hosts) {
     vector<string> args;
+    string command = type;
     if (type == "ssh") {
       args = {arg1 + host, arg2};
-    } else {
+    } else if (type == "term") {
+      args = {"-e", "ssh " + arg1 + host + " '" + arg2 + '\''};
+      command = getenv("COLORTERM");
+    } else {  // it's an scp or an rsync
       args = {arg1, arg2 + host + arg3};
     }
-    proc proc(host, type, args);
+    proc proc(host, command, args);
     procs.push_back(proc);
   }
 
   for (const proc& proc : procs) {
-    Poco::PipeInputStream i_str(proc.out_pipe_);
+    Poco::PipeInputStream out_stream(proc.out_pipe_);
     string out;
-    for (int c = i_str.get(); c != -1; c = i_str.get()) {
+    for (int c = out_stream.get(); c != -1; c = out_stream.get()) {
       out += static_cast<char>(c);
+    }
+
+    Poco::PipeInputStream err_stream(proc.err_pipe_);
+    string err;
+    for (int c = err_stream.get(); c != -1; c = err_stream.get()) {
+      err += static_cast<char>(c);
     }
 
     const int es = proc.proc_hand_.wait();
     const string color = es ? "\033[;31m" : "\033[;32m";
     cout << color << proc.host_ << " | "
-         << "es=" << es << "\033[m\n"
-         << out << "\n\n";
+         << "es=" << es << "\033[m\n";
+    cout << out << '\n';
+    cerr << err << '\n';
   }
 }
 
@@ -114,7 +128,7 @@ vector<string> split_arg_by_at(string& arg) {
   size_t at_pos = arg_unaltered.find('@');
   if (at_pos == string::npos) {
     at_pos = 0;
-  } else {
+  } else {  // no user specified
     arg = arg_unaltered.substr(0, ++at_pos);
   }
 
@@ -143,7 +157,7 @@ int main(const int argc, const char* argv[]) {
   if (type == "ssh" || type == "scp" || type == "rsync" || type == "term" ||
       type == "list") {
     args.erase(args.begin());
-  } else {
+  } else {  // ssh is the default type if nothing is specified
     type = "ssh";
   }
 
@@ -155,10 +169,10 @@ int main(const int argc, const char* argv[]) {
   string arg2 = *(++i);
 
   vector<string> hosts;
-  if (type == "ssh" || type == "list") {
+  if (type == "ssh" || type == "list" || type == "term") {
     hosts = split_arg_by_at(arg1);
     grp_cmd(type, arg1, arg2, hosts);
-  } else {
+  } else {  // it's an scp or an rsync
     const string arg3 = split_arg_by_colon(arg2);
     hosts = split_arg_by_at(arg2);
     grp_cmd(type, arg1, arg2, hosts, arg3);
